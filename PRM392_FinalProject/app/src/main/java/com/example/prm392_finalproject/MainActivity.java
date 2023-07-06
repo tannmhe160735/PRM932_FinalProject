7831218672919dae4ec4d8d4859b69ba03d31230
AnhTuanBaNguyen <101693374+AnhTuanBaNguyen@users.noreply.github.com>
2023-07-06 13:37:14 +0700

    Gun_skin_shop_update

--- PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/MainActivity.java
+++ PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_bundle);
+        setContentView(R.layout.activity_listbundle);
         bindingView();
         bindingAction();
         fakeData();
