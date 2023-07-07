4ec2678a4f2a1d54b04c3dead84c4ff0b645aefa
minhdao1809 <92740044+minhdao1809@users.noreply.github.com>
2023-07-07 21:51:30 +0700

    uxui

---
+++ PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.prm392_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
